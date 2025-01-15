208702997c15d673bc3523bba013e8a13b715abd
jarvis-codegeek <77537117+jarvis-codegeek@users.noreply.github.com>
2025-01-15 15:11:43 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -37,3 +37,27 @@ const DownloadButton = () => {
           This is a success message!
         </Alert>
       </Snackbar>
+.label-critical {
+            background-color: #dc3545; /* Red */
+            color: #fff;
+            padding: 0.2em 0.5em;
+            border-radius: 0.25em;
+        }
+        .label-high {
+            background-color: #ffc107; /* Yellow */
+            color: #000;
+            padding: 0.2em 0.5em;
+            border-radius: 0.25em;
+        }
+        .label-medium {
+            background-color: #17a2b8; /* Teal */
+            color: #fff;
+            padding: 0.2em 0.5em;
+            border-radius: 0.25em;
+        }
+        .label-low {
+            background-color: #28a745; /* Green */
+            color: #fff;
+            padding: 0.2em 0.5em;
+            border-radius: 0.25em;
+        }
