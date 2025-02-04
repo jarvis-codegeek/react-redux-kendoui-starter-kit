6b1168fec70811691ea531bc527aa782cd3b36cd
jarvis-codegeek <77537117+jarvis-codegeek@users.noreply.github.com>
2025-02-04 15:01:05 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,22 +1,22 @@
-SELECT 
-    i.id AS item_id,
-    i.name AS item_name,
-    i.version AS item_version,
-
+WITH limited_items AS (
+    SELECT 
+        i.id AS item_id,
+        i.name AS item_name,
+        i.version AS item_version
+    FROM items i
+    LIMIT 4500
+)
+SELECT
+    li.item_id,
+    li.item_name,
+    li.item_version,
     CASE 
         WHEN c.id IS NOT NULL OR s.id IS NOT NULL THEN 'Yes' 
         ELSE 'No' 
     END AS b_available
-
 FROM 
-    items i  -- Ensure we check only 5139 items
-
-LEFT JOIN 
-    ci_benchmarks c 
-ON 
-    similarity(i.name, c.title) > 0.5
-
-LEFT JOIN 
-    st_benchmarks s 
-ON 
-    similarity(i.name, s.title) > 0.5
+    limited_items li
+    LEFT JOIN c_benchmarks c 
+    ON similarity(li.item_name, c.benchmarktitle) > 0.5
+    LEFT JOIN s_benchmarks s 
+    ON similarity(li.item_name, s.benchmarktitle) > 0.5
